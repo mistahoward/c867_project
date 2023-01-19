d4cde8c0e7ffab5b32514ce0ed90bd3d7fedec60
mistahoward <me@alexhoward.dev>
2023-01-18 18:32:29 -0600

    Initial Commit

---
+++ c867_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c867_project
+//
+//  Created by Alex Howard on 1/18/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
